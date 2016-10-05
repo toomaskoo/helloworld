5c15aeda4f127b22a0660f6f0168e44beb4dcdfc
Toomas Koost <toomas@wizibe.com>
2016-10-05 14:23:27 +0300

    cows go moo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
+        System.out.println("Cows go moo when they poo");
     }
 }
